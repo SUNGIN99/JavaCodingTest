be12c06d213eb0089ec4cc7aec1dfb612fa23e76
SUNGIN99 <90676644+SUNGIN99@users.noreply.github.com>
2023-09-12 16:53:11 +0900

    [level 2] Title: 기능개발, Time: 2.16 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,14 +2,15 @@ import java.util.ArrayList;
 class Solution {
      public int[] solution(int[] progresses, int[] speeds) {
         int len = progresses.length;
-
+        // 길이가 1일때는 배포 하는날이 하루밖에 없음.
         if(len == 1){
             return new int[]{1};
         }
-
+        
+        // 결과를 담을 배포하는 날에 작업 수
         ArrayList<Integer> answer = new ArrayList<>();
 
-        // 남은 일수를 담기.
+        // 각 결과물의 작업속도 당 배포까지 남은 일 수를 구한다.
         int [] leftDays = new int[len];
         int integer, remain, leftday;
         for(int i = 0; i<len; i++){
@@ -23,7 +24,8 @@ class Solution {
             leftDays[i] = integer;
         }
 
-
+        // i번째 필요 작업 일 수가 i+1번째 작업의 작업 일 수보다 작다면, 배포가 먼저됨.
+        // i 번쨰 필요 작업 일 수가 i+1 번쨰 작업의 작업 일 수보다 크다면, 배포가 같이 됨.
         int count = 1, prev = leftDays[0];
         for(int i = 1; i<len; i++){
             if(prev >= leftDays[i]){
