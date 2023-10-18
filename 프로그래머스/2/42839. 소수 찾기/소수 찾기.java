77d95e1734b7da2ba3cd1292e5e460c17e3bd086
SUNGIN99 <90676644+SUNGIN99@users.noreply.github.com>
2023-10-19 00:04:24 +0900

    [level 2] Title: 소수 찾기, Time: 4.34 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -10,24 +10,32 @@ class Solution {
         return answer;
     }
     
+    // 모든 숫자 브르투 포스
     public void getNums(String numbers, String prime){
         for(int i = 0; i<numbers.length(); i++){
+            // 현재 만들 수 있는 숫자 = 이전에 만든 숫자 + 현재 index 숫자
             Integer numMade = Integer.parseInt(prime + numbers.substring(i,i+1));
+            
+            // 현재 숫자를 제외한 숫자 목록 설정
             String nextPrime = numbers.substring(0,i);
             
+            // 이미 탐색한 숫자인지 확인
             if(!hash.containsKey(numMade)){
                 hash.put(numMade, 0);
                 System.out.println(numMade);
                 
-                if(isPrime(numMade)){
+                if(isPrime(numMade)){// 소수 판별
                     answer++;
                 }                
             }
+            // 숫자 목록 나를 제외하고 추가 (만일 현재 숫자 뒤쪽이 문자열의 끝이 아닐때)
             if(i+1 < numbers.length()){
-                    nextPrime = nextPrime + numbers.substring(i+1, numbers.length());
+                nextPrime = nextPrime + numbers.substring(i+1, numbers.length());
+                // nextPrime = 0~i-1  + i+1 ~ len-1 (i만 제외)
             }
             
-            getNums(nextPrime, String.valueOf(numMade));
+            if(!nextPrime.equals(""))
+                getNums(nextPrime, String.valueOf(numMade));
         }
     }
     
