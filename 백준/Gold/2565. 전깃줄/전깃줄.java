0f9fe685bfc0d96c798cf23ae1eb5797688f763a
SUNGIN99 <90676644+SUNGIN99@users.noreply.github.com>
2025-02-05 23:34:51 +0900

    [Gold V] Title: 전깃줄, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,76 +1,117 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.*;
-import java.io.*;
 
 public class Main {
 
-    static class ElectricLine implements Comparable<ElectricLine>{
-        int start;
-        int end;
+    /*
+    8
+1 8
+3 9
+2 2
+4 1
+6 4
+10 10
+9 7
+7 6
 
-        ElectricLine(int start, int end){
-            this.start = start;
-            this.end = end;
-        }
+4
+1 4
+2 1
+3 2
+4 3
+     */
+    public static void main(String[] args) throws Exception {
 
-        public int compareTo(ElectricLine e){
-            return this.start - e.start;
-        }
-    }
-
-
-    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st; // = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-
-        ElectricLine[] lines = new ElectricLine[n];
+        int n = Integer.parseInt(br.readLine());
+        electrics = new int[n][];
         for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            lines[i] = new ElectricLine(start, end);
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            electrics[i] = new int[] {a, b};
+
         }
 
-        Arrays.sort(lines);
-
-        int[] lis = new int[n];
-        int max = 0;
-        for(int line = 0; line< n; line++){
-            lis[line] = 1;
-            for(int j = 0; j<line; j++){
-                if(lineUnCross(lines[line], lines[j])){
-                    lis[line] = Math.max(lis[line], lis[j] + 1);
-                }
+        // 앞에 전기줄을 모두 확인하기 위해
+        Arrays.sort(electrics, new Comparator<int[]> () {
+            public int compare(int[] a, int [] b){
+                return a[0] - b[0];
             }
-            
-            max = Math.max(max, lis[line]);
+        }) ;
+
+
+        for(int i = 0; i<n; i++){
+            //System.out.println(electrics[i][0] + ", " + electrics[i][1]);
+            binary1(electrics[i][0]);
+            binary2(electrics[i][1]);
         }
 
-        /*for(int i = 0; i<n; i++){
-            System.out.println(lines[i].start + ", " + lines[i].end + " : " + lis[i]);
-        }*/
+        //System.out.println(Arrays.toString(aa));
+        //System.out.println(Arrays.toString(bb));
 
-        System.out.println(n - max);
+        //System.out.println(lenA);
+        System.out.println(n - lenB);
+
     }
 
-    static boolean lineUnCross(ElectricLine e1, ElectricLine e2){
-        if(e1.start < e2.start){
-            if(e1.end < e2.end){ // (안 겹침)
-                return true;
-            }
-            else{ // e1.end > e2.end (겹침)
-                return false;
+    static int[][] electrics;
+    static int[] aa = new int[500];
+    static int lenA = 0;
+    static int[] bb = new int[500];
+    static int lenB = 0;
+    static void binary1(int a){
+        int left = 0;
+        int right = lenA;
+
+        while(left < right){
+            int mid = (left + right) / 2;
+
+            if(aa[mid] < a){
+                left = mid + 1;
+            }else if(aa[mid] > a){
+                right = mid;
+            }else{
+                break;
             }
         }
 
-        else { // e1.start > e2.start (둘이 같은 경우는 X)
-            if(e1.end < e2.end){ // (겹침)
-                return false;
-            }
-            else{  // (안 겹침)
-                return true;
+        if(right+1 > lenA){
+            lenA = right+1;
+        }
+
+        aa[right] = a;
+    }
+
+    static void binary2(int a){
+        int left = 0;
+        int right = lenB;
+
+        while(left < right){
+            int mid = (left + right) / 2;
+
+            if(bb[mid] < a){
+                left = mid + 1;
+            }else if(bb[mid] > a){
+                right = mid;
+            }else{
+                break;
             }
         }
+
+        if(right+1 > lenB){
+            lenB = right+1;
+        }
+
+        bb[right] = a;
     }
+
+
 }
